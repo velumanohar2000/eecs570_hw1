0f0b1295b879a696afb6c96bb7506679e0c540b3
Velu Manohar <velu@bane.eecs.umich.edu>
2025-01-29 19:23:14 -0500

    outer loop paralellized significantly faster

--- beamform.c
+++ beamform.c
@@ -18,6 +18,7 @@
 typedef struct thread_args{
     int start;
     int end;
+	float *image_temp;
 }thread_args;
 
 int sls_t; // Number of scanlines in theta
@@ -53,12 +54,14 @@ float *rx_data; // Pointer to pre-processed receive channel data
 
 int size;
 
+int total_angles;
+
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // with 8 threads we are able to double performace for transmit distance
 void *transmit_distance(void *arg){
 
-	thread_args *range = (struct thread_args *) arg;
+	thread_args *thread_info = (struct thread_args *) arg;
 
 	/* First compute transmit distance */
 	int point;
@@ -70,70 +73,57 @@ void *transmit_distance(void *arg){
 	float x_comp; // Itermediate value for dist calc
 	float y_comp; // Itermediate value for dist calc
 	float z_comp; // Itermediate value for dist calc
-	int k;
+	int it_angle;
 
-	point = range->start * size * pts_r;
-	//printf("range->start: %d point: %d\n", range->start, point);
-	//for (it_t = 0; it_t < sls_t; it_t++) {
-	for(it_t = range->start; it_t < range->end; it_t++) {
-		for (it_p = 0; it_p < sls_p; it_p++) {
-			for (it_r = 0; it_r < pts_r; it_r++) {
+	point = thread_info->start * pts_r;
 
-				x_comp = tx_x - point_x[point];
-				x_comp = x_comp * x_comp;
-				y_comp = tx_y - point_y[point];
-				y_comp = y_comp * y_comp;
-				z_comp = tx_z - point_z[point];
-				z_comp = z_comp * z_comp;
+	for(it_angle = thread_info->start; it_angle < thread_info->end; it_angle++) {
+		for (it_r = 0; it_r < pts_r; it_r++) {
 
-				dist_tx[point++] = (float)sqrt(x_comp + y_comp + z_comp);
-			}
+			x_comp = tx_x - point_x[point];
+			x_comp = x_comp * x_comp;
+			y_comp = tx_y - point_y[point];
+			y_comp = y_comp * y_comp;
+			z_comp = tx_z - point_z[point];
+			z_comp = z_comp * z_comp;
+
+			dist_tx[point++] = (float)sqrt(x_comp + y_comp + z_comp);
 		}
 	}
 }
 
+
 void *reflect_distance(void *arg){
 /* Now compute reflected distance, find index values, add to image */
 
-	thread_args *range = (struct thread_args *) arg;
+	thread_args *thread_info = (struct thread_args *) arg;
 
 	int point = 0;
 	int it_rx; // Iterator for recieve transducer
-
 	int it_t; // Iterator for theta
 	int it_p; // Iterator for phi
 	int it_r; // Iterator for r
-
 	int index; // Index into transducer data
-
 	float x_comp; // Itermediate value for dist calc
 	float y_comp; // Itermediate value for dist calc
 	float z_comp; // Itermediate value for dist calc
-
 	float dist;
 	int offset = 0;
 	float *image_pos; // Pointer to current position in image
+	float *image_temp = thread_info->image_temp;
 
+	offset = thread_info->start * data_len;
+	for (it_rx = thread_info->start; it_rx < thread_info->end; it_rx++) {  // 1024 times
 
-	//for (it_rx = 0; it_rx < trans_x * trans_y; it_rx++) {  // 1024 times
-	offset = range->start * data_len;
-	// float *image_temp = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
-	// memset(image_temp, 0, pts_r * sls_t * sls_p * sizeof(float));
-
-	// float *original_image_temp = image_temp;
-
-
-
-	for (it_rx = range->start; it_rx < range->end; it_rx++) {  // 1024 times
-
-		image_pos = image; // Reset image pointer back to beginning
+		//image_pos = image; // Reset image pointer back to beginning
 		point = 0;
-		//image_temp = original_image_temp;
+		image_temp = thread_info->image_temp;
+
 		// Iterate over entire image space
 		for (it_t = 0; it_t < sls_t; it_t++) {    // whatever size is
 			for (it_p = 0; it_p < sls_p; it_p++) { // whatever size is
 				for (it_r = 0; it_r < pts_r; it_r++) { //1056
-
+					
 					x_comp = rx_x[it_rx] - point_x[point];
 					x_comp = x_comp * x_comp;
 					y_comp = rx_y[it_rx] - point_y[point];
@@ -144,12 +134,8 @@ void *reflect_distance(void *arg){
 
 					dist = dist_tx[point++] + (float)sqrt(x_comp + y_comp + z_comp); // change to local
 					index = (int)(dist/idx_const + filter_delay + 0.5);
-					// *image_temp = rx_data[index+offset];
-					// image_temp++;
-					pthread_mutex_lock(&lock);
-					*image_pos += rx_data[index+offset];
-					image_pos++;
-					pthread_mutex_unlock(&lock);
+					*image_temp += rx_data[index+offset];
+					image_temp++;
 
 				}
 			}
@@ -157,117 +143,140 @@ void *reflect_distance(void *arg){
 		offset += data_len;
 	}
 
-	// pthread_mutex_lock(&lock);
-	// *image_pos += rx_data[index+offset];
-	// image_pos++;
-	// pthread_mutex_unlock(&lock);
-
 }
 
 
+void allocate_space()
+{
+	
+	/* Allocate space for data */
+	rx_x = (float*) malloc(trans_x * trans_y * sizeof(float));
+	if (rx_x == NULL) fprintf(stderr, "Bad malloc on rx_x\n");
+	rx_y = (float*) malloc(trans_x * trans_y * sizeof(float));
+	if (rx_y == NULL) fprintf(stderr, "Bad malloc on rx_y\n");
+	rx_data = (float*) malloc(data_len * trans_x * trans_y * sizeof(float));
+	if (rx_data == NULL) fprintf(stderr, "Bad malloc on rx_data\n");
 
+	point_x = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
+	if (point_x == NULL) fprintf(stderr, "Bad malloc on point_x\n");
+	point_y = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
+	if (point_y == NULL) fprintf(stderr, "Bad malloc on point_y\n");
+	point_z = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
+	if (point_z == NULL) fprintf(stderr, "Bad malloc on point_z\n");
+
+	dist_tx = (float*) malloc(pts_r * sls_t * sls_p * sizeof(float));
+	if (dist_tx == NULL) fprintf(stderr, "Bad malloc on dist_tx\n");
+
+	image = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
+	if (image == NULL) fprintf(stderr, "Bad malloc on image\n");
+	memset(image, 0, pts_r * sls_t * sls_p * sizeof(float));
+
+}
+
+void read_binary(FILE *input)
+{
+	/* Load data from binary */
+	fread(rx_x, sizeof(float), trans_x * trans_y, input); 
+	fread(rx_y, sizeof(float), trans_x * trans_y, input); 
+
+	fread(point_x, sizeof(float), pts_r * sls_t * sls_p, input); 
+	fread(point_y, sizeof(float), pts_r * sls_t * sls_p, input); 
+	fread(point_z, sizeof(float), pts_r * sls_t * sls_p, input); 
+
+	fread(rx_data, sizeof(float), data_len * trans_x * trans_y, input); 
+	fclose(input);
+}
 
 int main (int argc, char **argv) {
 
 	size = atoi(argv[1]);
 
 
+
 	/* Variables for image space points */
 	sls_t = size; // Number of scanlines in theta
 	sls_p = size; // Number of scanlines in phi
+	total_angles = sls_p * sls_t;
+
+	allocate_space();
+
 
     FILE* input;
     FILE* output;
 
-	/* Allocate space for data */
-		rx_x = (float*) malloc(trans_x * trans_y * sizeof(float));
-		if (rx_x == NULL) fprintf(stderr, "Bad malloc on rx_x\n");
-		rx_y = (float*) malloc(trans_x * trans_y * sizeof(float));
-		if (rx_y == NULL) fprintf(stderr, "Bad malloc on rx_y\n");
-		rx_data = (float*) malloc(data_len * trans_x * trans_y * sizeof(float));
-		if (rx_data == NULL) fprintf(stderr, "Bad malloc on rx_data\n");
-
-		point_x = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
-		if (point_x == NULL) fprintf(stderr, "Bad malloc on point_x\n");
-		point_y = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
-		if (point_y == NULL) fprintf(stderr, "Bad malloc on point_y\n");
-		point_z = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
-		if (point_z == NULL) fprintf(stderr, "Bad malloc on point_z\n");
-
-		dist_tx = (float*) malloc(pts_r * sls_t * sls_p * sizeof(float));
-		if (dist_tx == NULL) fprintf(stderr, "Bad malloc on dist_tx\n");
-
-		image = (float *) malloc(pts_r * sls_t * sls_p * sizeof(float));
-		if (image == NULL) fprintf(stderr, "Bad malloc on image\n");
-		memset(image, 0, pts_r * sls_t * sls_p * sizeof(float));
-
-		/* validate command line parameter */
+	// read cmd line input
 		if (argc < 1 || !(strcmp(argv[1],"16") || strcmp(argv[1],"32") || strcmp(argv[1],"64"))) {
-		printf("Usage: %s {16|32|64}\n",argv[0]);
-		fflush(stdout);
-		exit(-1);
+				printf("Usage: %s {16|32|64}\n",argv[0]);
+				fflush(stdout);
+				exit(-1);
 		}
-
 		char buff[128];
 			#ifdef __MIC__
 		sprintf(buff, "/beamforming_input_%s.bin", argv[1]);
 			#else // !__MIC__
 		sprintf(buff, "/n/typhon/data1/home/eecs570/beamforming_input_%s.bin", argv[1]);
 			#endif
-
-			input = fopen(buff,"rb");
+		input = fopen(buff,"rb");
 		if (!input) {
-		printf("Unable to open input file %s.\n", buff);
-		fflush(stdout);
-		exit(-1);
+			printf("Unable to open input file %s.\n", buff);
+			fflush(stdout);
+			exit(-1);
 		}	
-
-		/* Load data from binary */
-		fread(rx_x, sizeof(float), trans_x * trans_y, input); 
-		fread(rx_y, sizeof(float), trans_x * trans_y, input); 
-
-		fread(point_x, sizeof(float), pts_r * sls_t * sls_p, input); 
-		fread(point_y, sizeof(float), pts_r * sls_t * sls_p, input); 
-		fread(point_z, sizeof(float), pts_r * sls_t * sls_p, input); 
-
-		fread(rx_data, sizeof(float), data_len * trans_x * trans_y, input); 
-			fclose(input);
-
-		printf("Beginning computation\n");
-		fflush(stdout);
-
-
 	//
-	
-	
-	
-	pthread_t transmit_threads[NUM_THREADS_TRANSMIT];
-	thread_args transmit_work_ranges[NUM_THREADS_TRANSMIT];
 
-	pthread_t reflect_threads[NUM_THREADS_REFLECT];
-	thread_args reflect_work_ranges[NUM_THREADS_REFLECT];
+	read_binary(input);
+
+
+	printf("Beginning computation\n");
+	fflush(stdout);
+
+	
+	
+	
+	
+
+	
 
     int current_start, range;
 	int i = 0;
+	int j = 0;
+
+	// Transmit thread init
+	pthread_t transmit_threads[NUM_THREADS_TRANSMIT];
+	thread_args transmit_work_ranges[NUM_THREADS_TRANSMIT];
 
     current_start = 0;
-    range = size / NUM_THREADS_TRANSMIT;
+    range = total_angles / NUM_THREADS_TRANSMIT;
     for(i = 0; i < NUM_THREADS_TRANSMIT; i++) {
         transmit_work_ranges[i].start = current_start;
         transmit_work_ranges[i].end = current_start + range;
         current_start += range;
     }
-    transmit_work_ranges[NUM_THREADS_TRANSMIT-1].end = size;
+    transmit_work_ranges[NUM_THREADS_TRANSMIT-1].end = total_angles;
+
+	// Reflect Thread init
+	pthread_t reflect_threads[NUM_THREADS_REFLECT];
+	thread_args reflect_work_ranges[NUM_THREADS_REFLECT];
+	float **temp_images = (float **)malloc(NUM_THREADS_REFLECT * sizeof(float *));
+
 
 	current_start = 0;
     range = 1024 / NUM_THREADS_REFLECT;
     for(i = 0; i < NUM_THREADS_REFLECT; i++) {
         reflect_work_ranges[i].start = current_start;
         reflect_work_ranges[i].end = current_start + range;
-        current_start += range;
+
+		temp_images[i] = (float *)malloc(pts_r * sls_t * sls_p * sizeof(float));
+		if (temp_images[i] == NULL) fprintf(stderr, "Bad malloc on temp_images[%d]\n", i);
+		memset(temp_images[i], 0, pts_r * sls_t * sls_p * sizeof(float));
+
+		reflect_work_ranges[i].image_temp = temp_images[i];
+
+		current_start += range;
     }
     reflect_work_ranges[NUM_THREADS_REFLECT-1].end = 1024;
 
+	
 	
 
 
@@ -298,6 +307,15 @@ int main (int argc, char **argv) {
     for(i = 0; i < NUM_THREADS_REFLECT; i++) {
         pthread_join(reflect_threads[i], NULL);
     }
+
+	    // Combine temporary images into the final image
+    for (i = 0; i < NUM_THREADS_REFLECT; i++) {
+        for (j = 0; j < pts_r * sls_t * sls_p; j++) {
+            image[j] += temp_images[i][j];
+        }
+        free(temp_images[i]);
+    }
+	free(temp_images);
 
 	
 
